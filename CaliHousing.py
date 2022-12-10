b8191b36a455e78ea2fd544fecaa812966cf49fa
ThuyyyTranggg <114237753+ThuyyyTranggg@users.noreply.github.com>
2022-12-10 19:33:35 +0700

    Add files via upload

---
+++ CaliHousing.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pydeck as pdk
+import source.End_to_End_Project.CaliHousing.Decision_Tree_Regression as bai1
+import source.End_to_End_Project.CaliHousing.Linear_Regression_UseModel as bai2
+import source.End_to_End_Project.CaliHousing.Linear_Regression as bai3
+import source.End_to_End_Project.CaliHousing.Random_Forest_Regression_Grid_Search_CV as bai4
+import source.End_to_End_Project.CaliHousing.Random_Forest_Regression_Random_Search_CV_UseModel as bai5
+import source.End_to_End_Project.CaliHousing.Random_Forest_Regression_Random_Search_CV as bai6
+import source.End_to_End_Project.CaliHousing.Random_Forest_Regression as bai7
+
+st.title("CaliHousing")
+check = st.checkbox("Bảng dữ liệu")
+if (check):
+    st.dataframe(bai1.housing)
+
+menu = st.sidebar.selectbox("menu", ["Decision_Tree_Regression", "Linear_Regression_UseModel", "Linear_Regression", "Random_Forest_Regression_Grid_Search_CV", "Random_Forest_Regression_Random_Search_CV_UseModel", "Random_Forest_Regression_Random_Search_CV", "Random_Forest_Regression"])
+
+if menu == "Decision_Tree_Regression":
+    st.line_chart(bai1.housing_labels)
+    st.write("Sai số bình phương trung bình - train: ", '%.2f' % bai1.rmse_train)
+    st.write("Sai số bình phương trung bình - cross-valid: ")
+    st.write("Sai số bình phương trung bình - test: ", '%.2f' % bai1.rmse_test)
+
+if menu == "Linear_Regression_UseModel":
+    st.subheader("Predictions")
+    st.line_chart(bai2.pre)
+    st.write("Sai số bình phương trung bình - train: ", '%.2f' % bai2.saiSo_train)
+    st.write("Sai số bình phương trung bình - test: ", '%.2f' % bai2.saiSo_test)
+    st.line_chart(bai2.y_predictions)
+if menu== "Linear_Regression":
+    st.subheader("Predictions")
+    st.line_chart(bai3.pre1)
+    st.write("Sai số bình phương trung bình - train: ", '%.2f' % bai3.saiSo_train)
+    st.write("Sai số bình phương trung bình - test:", '%.2f' % bai3.saiSo_test)
+if menu == "Random_Forest_Regression_Grid_Search_CV":
+    st.write("Sai số bình phương trung binh - train:", '%.2f' % bai4.rmse_train)
+    st.write("Sai số bình phương trung bình - test:", '%.2f' % bai4.rmse_test)
+if menu == "Random_Forest_Regression_Random_Search_CV_UseModel":
+    st.write("Sai số bình phương trung binh - train:", '%.2f' % bai5.rmse_train)
+    st.write("Sai số bình phương trung bình - test:", '%.2f' % bai5.rmse_test)
+if menu == "Random_Forest_Regression_Random_Search_CV":
+    st.write("Sai số bình phương trung binh - train:", '%.2f' % bai6.rmse_train)
+    st.write("Sai số bình phương trung bình - test:", '%.2f' % bai6.rmse_test) 
+if menu == "Random_Forest_Regression":
+    st.write("Sai số bình phương trung binh - train:", '%.2f' % bai7.rmse_train)
+    st.write("Sai số bình phương trung bình - test:", '%.2f' % bai7.rmse_test) 
---
+++ GiamDanDaoHam.py
@@ -0,0 +1,329 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from matplotlib import pyplot as plt
+
+st.title("GIẢM DẦN - ĐẠO HÀM")
+
+tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(["bai01", "bai02", "Bai02a", "Bai03", "bai04", "bai05", "temp"])
+with tab1:
+    def grad(x):
+        return 2*x+ 5*np.cos(x)
+    def cost(x):
+        return x**2 + 5*np.sin(x)
+
+    def myGD1(x0, eta):
+        x = [x0]
+        for it in range(100):
+            x_new = x[-1] - eta*grad(x[-1])
+            if abs(grad(x_new)) < 1e-3: # just a small number
+                break
+            x.append(x_new)
+        return (x, it)
+
+    x0 = -5
+    eta = 0.1
+    (x, it) = myGD1(x0, eta)
+    x = np.array(x)
+    y = cost(x)
+
+    n = 101
+    xx = np.linspace(-6, 6, n)
+    yy = xx**2 + 5*np.sin(xx)
+    z = [xx, yy]
+    fig = plt.subplot(2,4,1)
+    plt.plot(xx,yy)
+    index = 0
+    plt.plot(x[index], y[index], 'ro')
+    s = ' iter%d/%d, grad=%.3f ' % (index, it, grad(x[index]))
+    plt.xlabel(s, fontsize = 8)
+    plt.axis([-7, 7, -10, 50])
+
+
+
+    plt.subplot(2,4,2)
+    plt.plot(xx, yy)
+    index = 1
+    plt.plot(x[index], y[index], 'ro')
+    s = ' iter%d/%d, grad=%.3f ' % (index, it, grad(x[index]))
+    plt.xlabel(s, fontsize = 8)
+    plt.axis([-7, 7, -10, 50])
+
+    plt.subplot(2,4,3)
+    plt.plot(xx, yy)
+    index = 2
+    plt.plot(x[index], y[index], 'ro')
+    s = ' iter%d/%d, grad=%.3f ' % (index, it, grad(x[index]))
+    plt.xlabel(s, fontsize = 8)
+    plt.axis([-7, 7, -10, 50])
+
+    plt.subplot(2,4,4)
+    plt.plot(xx, yy)
+    index = 3
+    plt.plot(x[index], y[index], 'ro')
+    s = ' iter%d/%d, grad=%.3f ' % (index, it, grad(x[index]))
+    plt.xlabel(s, fontsize = 8)
+    plt.axis([-7, 7, -10, 50])
+
+    plt.subplot(2,4,5)
+    plt.plot(xx, yy)
+    index = 4
+    plt.plot(x[index], y[index], 'ro')
+    s = ' iter%d/%d, grad=%.3f ' % (index, it, grad(x[index]))
+    plt.xlabel(s, fontsize = 8)
+    plt.axis([-7, 7, -10, 50])
+
+    plt.subplot(2,4,6)
+    plt.plot(xx, yy)
+    index = 5
+    plt.plot(x[index], y[index], 'ro')
+    s = ' iter%d/%d, grad=%.3f ' % (index, it, grad(x[index]))
+    plt.xlabel(s, fontsize = 8)
+    plt.axis([-7, 7, -10, 50])
+
+    plt.subplot(2,4,7)
+    plt.plot(xx, yy)
+    index = 7
+    plt.plot(x[index], y[index], 'ro')
+    s = ' iter%d/%d, grad=%.3f ' % (index, it, grad(x[index]))
+    plt.xlabel(s, fontsize = 8)
+    plt.axis([-7, 7, -10, 50])
+
+    plt.subplot(2,4,8)
+    plt.plot(xx, yy)
+    index = 11
+    plt.plot(x[index], y[index], 'ro')
+    s = ' iter%d/%d, grad=%.3f ' % (index, it, grad(x[index]))
+    plt.xlabel(s, fontsize = 8)
+    plt.axis([-7, 7, -10, 50])
+
+    plt.tight_layout()
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.pyplot()
+    
+with tab2:
+    X = np.random.rand(1000)
+
+    y = 4 + 3 * X + .5*np.random.randn(1000)
+
+    model = LinearRegression()
+    model.fit(X.reshape(-1, 1), y.reshape(-1, 1))
+    w, b = model.coef_[0][0], model.intercept_[0]
+    x0 = 0
+    x1 = 1
+    y0 = w*x0 + b
+    y1 = w*x1 + b
+
+    z = [x0, x1]
+    t = [y0, y1]
+    plt.plot(X, y, 'bo', markersize = 2)
+    b = plt.plot([x0, x1], [y0, y1], 'r')
+
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.pyplot()
+with tab3:
+    X = np.random.rand(1000)
+    y = 4 + 3 * X + .5*np.random.randn(1000) # noise added
+
+    model = LinearRegression()
+    model.fit(X.reshape(-1, 1), y.reshape(-1, 1))
+
+    w, b = model.coef_[0][0], model.intercept_[0]
+    sol_sklearn = np.array([b, w])
+    print('Solution found by sklearn:', sol_sklearn)
+
+    # Building Xbar 
+    one = np.ones((X.shape[0],1))
+    Xbar = np.concatenate((one, X.reshape(-1, 1)), axis = 1)
+
+    def grad(w):
+        N = Xbar.shape[0]
+        return 1/N * Xbar.T.dot(Xbar.dot(w) - y)
+
+    def cost(w):
+        N = Xbar.shape[0]
+        return .5/N*np.linalg.norm(y - Xbar.dot(w))**2
+
+    def myGD(w_init, eta):
+        w = [w_init]
+        for it in range(100):
+            w_new = w[-1] - eta*grad(w[-1])
+            if np.linalg.norm(grad(w_new))/len(w_new) < 1e-3:
+                break 
+            w.append(w_new)
+        return (w, it)
+
+    w_init = np.array([0, 0])
+    (w1, it1) = myGD(w_init, 1)
+
+    st.write("Sol dound by GD: w = ", w1[-1], ',\nafter %d iterations.' %(it1+1))
+    print('Sol found by GD: w = ', w1[-1], ',\nafter %d iterations.' %(it1+1))
+
+with tab4:
+    np.random.seed(100)
+    N = 1000
+    X = np.random.rand(N)
+    y = 4 + 3 * X + .5*np.random.randn(N)
+
+    model = LinearRegression()
+    model.fit(X.reshape(-1, 1), y.reshape(-1, 1))
+    w, b = model.coef_[0][0], model.intercept_[0]
+    print('b = %.4f va w = %.4f' % (b, w))
+
+    one = np.ones((X.shape[0],1))
+    Xbar = np.concatenate((one, X.reshape(-1, 1)), axis = 1)
+
+    def grad(w):
+        N = Xbar.shape[0]
+        return 1/N * Xbar.T.dot(Xbar.dot(w) - y)
+
+    def cost(w):
+        N = Xbar.shape[0]
+        return .5/N*np.linalg.norm(y - Xbar.dot(w))**2
+
+    def myGD(w_init, eta):
+        w = [w_init]
+        for it in range(100):
+            w_new = w[-1] - eta*grad(w[-1])
+            if np.linalg.norm(grad(w_new))/len(w_new) < 1e-3:
+                break 
+            w.append(w_new)
+        return (w, it)
+
+    w_init = np.array([0, 0])
+    (w1, it1) = myGD(w_init, 1)
+    print('Sol found by GD: w = ', w1[-1], ',\nafter %d iterations.' %(it1+1))
+    for item in w1:
+        print(item, cost(item))
+
+    print(len(w1))
+
+    A = N/(2*N)
+    B = np.sum(X*X)/(2*N)
+    C = -np.sum(y)/(2*N)
+    D = -np.sum(X*y)/(2*N)
+    E = np.sum(X)/(2*N)
+    F = np.sum(y*y)/(2*N)
+
+    b = np.linspace(0,6,21)
+    w = np.linspace(0,6,21)
+    b, w = np.meshgrid(b, w)
+    z = A*b*b + B*w*w + C*b*2 + D*w*2 + E*b*w*2 + F
+
+    temp = w1[0]
+    bb = temp[0]
+    ww = temp[1]
+    zz = cost(temp) 
+    ax = plt.axes(projection="3d")
+    ax.plot3D(bb, ww, zz, 'ro', markersize = 3)
+
+    temp = w1[1]
+    bb = temp[0]
+    ww = temp[1]
+    zz = cost(temp) 
+    ax.plot3D(bb, ww, zz, 'ro', markersize = 3)
+
+    temp = w1[2]
+    bb = temp[0]
+    ww = temp[1]
+    zz = cost(temp) 
+    ax.plot3D(bb, ww, zz, 'ro', markersize = 3)
+
+    temp = w1[3]
+    bb = temp[0]
+    ww = temp[1]
+    zz = cost(temp) 
+    ax.plot3D(bb, ww, zz, 'ro', markersize = 3)
+
+
+    ax.plot_wireframe(b, w, z)
+    ax.set_xlabel("b")
+    ax.set_ylabel("w")
+    plt.show()
+    st.pyplot()
+
+with tab5:
+    x = np.linspace(-2, 2, 21)
+    y = np.linspace(-2, 2, 21)
+    X, Y = np.meshgrid(x, y)
+    Z = X**2 + Y**2
+    plt.contour(X, Y, Z, 10)
+    plt.show()
+    st.pyplot()
+with tab6:
+    np.random.seed(100)
+    N = 1000
+    X = np.random.rand(N)
+    y = 4 + 3 * X + .5*np.random.randn(N)
+
+    model = LinearRegression()
+    model.fit(X.reshape(-1, 1), y.reshape(-1, 1))
+    w, b = model.coef_[0][0], model.intercept_[0]
+    print('b = %.4f va w = %.4f' % (b, w))
+
+    one = np.ones((X.shape[0],1))
+    Xbar = np.concatenate((one, X.reshape(-1, 1)), axis = 1)
+
+    def grad(w):
+        N = Xbar.shape[0]
+        return 1/N * Xbar.T.dot(Xbar.dot(w) - y)
+
+    def cost(w):
+        N = Xbar.shape[0]
+        return .5/N*np.linalg.norm(y - Xbar.dot(w))**2
+
+    def myGD(w_init, eta):
+        w = [w_init]
+        for it in range(100):
+            w_new = w[-1] - eta*grad(w[-1])
+            if np.linalg.norm(grad(w_new))/len(w_new) < 1e-3:
+                break 
+            w.append(w_new)
+        return (w, it)
+
+    w_init = np.array([0, 0])
+    (w1, it1) = myGD(w_init, 1)
+    print('Sol found by GD: w = ', w1[-1], ',\nafter %d iterations.' %(it1+1))
+    for item in w1:
+        print(item, cost(item))
+
+    print(len(w1))
+
+    A = N/(2*N)
+    B = np.sum(X*X)/(2*N)
+    C = -np.sum(y)/(2*N)
+    D = -np.sum(X*y)/(2*N)
+    E = np.sum(X)/(2*N)
+    F = np.sum(y*y)/(2*N)
+
+    b = np.linspace(0,6,21)
+    w = np.linspace(0,6,21)
+    b, w = np.meshgrid(b, w)
+    z = A*b*b + B*w*w + C*b*2 + D*w*2 + E*b*w*2 + F
+
+    plt.contour(b, w, z, 45)
+    bdata = []
+    wdata = []
+    for item in w1:
+        plt.plot(item[0], item[1], 'ro', markersize = 3)
+        bdata.append(item[0])
+        wdata.append(item[1])
+
+    plt.plot(bdata, wdata, color = 'b')
+
+    plt.xlabel('b')
+    plt.ylabel('w')
+    plt.axis('square')
+    plt.show()
+    st.pyplot()
+with tab7:
+    ax = plt.axes(projection="3d")
+
+    X = np.linspace(-2, 2, 21)
+    Y = np.linspace(-2, 2, 21)
+    X, Y = np.meshgrid(X, Y)
+    Z = X*X + Y*Y
+    ax.plot_wireframe(X, Y, Z)
+    plt.show()
+    st.pyplot()
