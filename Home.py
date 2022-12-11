bafc15d8c379968a86a646006adeb38daa5df94c
nna9220 <nna9220@gmail.com>
2022-12-11 23:37:12 +0700

    fix Home

--- Home.py
+++ Home.py
@@ -2,7 +2,11 @@ import streamlit as st
 page_bg_img = """
 <h1 style= " margin-left:28vw; margin-top:0px;;color:red">ĐỒ ÁN CUỐI KÌ</h1>
 <h3 style="margin-left:28vw;">MACHINE LEARNING</h3>
-<h3 style="margin-left:28vw;">GVHD : Trần Tiến Đức </h3>
+<h4 style="margin-left:28vw;">GVHD : Ths.Trần Tiến Đức </h4>
+<h4 style="margin-left:28vw;">Lớp  : MALE431984_22_1_04 </h4>
+<h4 style="margin-left:28vw;">SVTH :</h4>
+<h6 style="margin-left:28vw;">1. Nguyễn Thị Na         20110678 </h6>
+<h6 style="margin-left:28vw;">2. Nguyễn Thị Thùy Trang 20110753 </h6>
 <br>
 <style>
 [data-testid="stAppViewContainer"] {
